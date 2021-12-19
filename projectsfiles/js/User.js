df825d28f1ef3367a4de04c9e8976f33e270408b
MaxDudnik32 <maxim.dudnik32@gmail.com>
2021-12-19 15:40:10 +0300

    Update User.js

--- projectsfiles/js/User.js
+++ projectsfiles/js/User.js
@@ -1,7 +1,8 @@
-function User(name, email, password, products, post) {
+function User(name, email, password, products, post, char) {
     this.name = name;
     this.email = email;
     this.password = password;
     this.products = products;
     this.post = post;
+    this.char = char;
 }
