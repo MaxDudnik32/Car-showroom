9c6e3daabbef7fc3e1fbeae6ab37c5031fdbc600
MaxDudnik32 <maxim.dudnik32@gmail.com>
2022-01-04 20:21:03 +0300

    Update users.js

--- projectsfiles/js/users.js
+++ projectsfiles/js/users.js
@@ -1,5 +1,5 @@
 const users = [
-    new User ('Jack', 'example@mail.com', 'qwertyewq321', 4, false, new Manager('manager', 0)),
-    new User ('Bob', 'example@mail.com', 'qwertyewq321', 5, true, 'user'),
-    new User ('Frank', 'example@mail.com', 'qwertyewq321', 6, true, 'user'),
+    new User ('Jack', 'example@mail.com', 'qwertyewq321', true),
+    new User ('Bob', 'example@mail.com', 'qwertyewq321', true),
+    new User ('Frank', 'example@mail.com', 'qwertyewq321', true),
 ]
