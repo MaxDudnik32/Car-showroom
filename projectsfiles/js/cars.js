a37537588dfd779095148fca39218a67952bc0b8
MaxDudnik32 <maxim.dudnik32@gmail.com>
2022-01-17 17:28:24 +0300

    Update cars.js

--- projectsfiles/js/cars.js
+++ projectsfiles/js/cars.js
@@ -1,26 +1,26 @@
 const cars = [
     new Car('Bmw',[
         new Details('M3', 40000, 15),
-        new Details('Motor', 10000, 17),
+        new Details('Engine', 10000, 17),
     ]),
     new Car('Volvo',[
-        new Details('M3', 50000, 13),
-        new Details('Motor', 10000, 7),
+        new Details('xc60', 50000, 9),
+        new Details('Engine', 10000, 7),
     ]),
     new Car('Mazda',[
-        new Details('M3', 40000, 15),
-        new Details('Motor', 10000, 17),
+        new Details('3', 40000, 5),
+        new Details('Engine', 10000, 17),
     ]),
     new Car('Hundai',[
-        new Details('M3', 40000, 15),
-        new Details('Motor', 10000, 17),
+        new Details('Sonata', 40000, 8),
+        new Details('Engine', 10000, 17),
     ]),
     new Car('Chevrolet',[
-        new Details('M3', 40000, 15),
-        new Details('Motor', 10000, 17),
+        new Details('Camaro', 40000, 12),
+        new Details('Engine', 10000, 17),
     ]),
     new Car('Mercedes',[
-        new Details('M3', 40000, 15),
-        new Details('Motor', 10000, 17),
+        new Details('Gl', 20000, 10),
+        new Details('Engine', 10000, 17),
     ]),
 ]
